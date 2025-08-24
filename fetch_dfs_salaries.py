6bea904849d3dec9cfb2b0d7beb9834dc5c4ba9c
gmurr20 <greg.murray20@gmail.com>
2025-08-24 12:51:01 -0500

    Add API fetching

---
+++ fetch_dfs_salaries.py
@@ -0,0 +1,19 @@
+import http.client
+import json
+from env_keys import RAPID_API
+
+conn = http.client.HTTPSConnection("tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': RAPID_API,
+    'x-rapidapi-host': "tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com"
+}
+
+conn.request("GET", "/getNFLDFS?date=20250119", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+json_data = json.loads(data.decode("utf-8"))
+with open('dfs_salaries.json', 'w', encoding='utf-8') as f:
+    json.dump(json_data, f, indent=2, ensure_ascii=False)
---
+++ fetch_ff_projections.py
@@ -0,0 +1,19 @@
+import http.client
+import json
+from env_keys import RAPID_API
+
+conn = http.client.HTTPSConnection("tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': RAPID_API,
+    'x-rapidapi-host': "tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com"
+}
+
+conn.request("GET", "/getNFLProjections?week=5&archiveSeason=2025&twoPointConversions=2&passYards=.04&passAttempts=-.5&passTD=4&passCompletions=1&passInterceptions=-2&pointsPerReception=1&carries=.2&rushYards=.1&rushTD=6&fumbles=-2&receivingYards=.1&receivingTD=6&targets=.1&fgMade=3&fgMissed=-1&xpMade=1&xpMissed=-1", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+json_data = json.loads(data.decode("utf-8"))
+with open('ff_projections.json', 'w', encoding='utf-8') as f:
+    json.dump(json_data, f, indent=2, ensure_ascii=False)
