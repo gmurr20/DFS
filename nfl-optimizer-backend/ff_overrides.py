45499e60cb146f2ddbc0763690c8d20299046cc0
gmurr20 <greg.murray20@gmail.com>
2025-09-24 18:58:02 -0500

    Week 4

--- nfl-optimizer-backend/ff_overrides.py
+++ nfl-optimizer-backend/ff_overrides.py
@@ -1,16 +1,4 @@
 
 
 player_to_pts_override = {
-    # Jakobi Meyers
-    "3916433": 13.8,
-    # Calvin Ridley
-    "3925357": 12.6,
-    # Bijan
-    "4430807": 21.1,
-    # CMC
-    "3117251": 22.5,
-    # Jordan Mason
-    "4360569": 17.5,
-    # K Walker
-    "4567048": 17.4,
 }
