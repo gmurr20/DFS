1dfd7c673c7567421e254d5b2a870cc659fe898c
gmurr20 <greg.murray20@gmail.com>
2025-10-04 17:48:03 -0500

    Update players

--- nfl-optimizer-backend/ff_overrides.py
+++ nfl-optimizer-backend/ff_overrides.py
@@ -1,2 +1,20 @@
 player_to_pts_override = {
+    # G Wilson
+    "4569618": 16.5,
+    # T McMillan
+    "4685472": 14.5,
+    # M Harrsion Jr
+    "4432708": 13.2,
+    # Calvin Ridley
+    "3925357": 9.0,
+    # Shaheed
+    "4032473": 9.5,
+    # Sun god
+    "4374302": 18.0,
+    # J Taylor
+    "4242335": 20.2,
+    # Gibbs
+    "4429795": 20.5,
+    # Cook
+    "4379399": 17.9,
 }
