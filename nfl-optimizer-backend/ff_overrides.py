3a69cc035274cdca36b8c258779a18586e18d320
gmurr20 <greg.murray20@gmail.com>
2025-10-11 19:25:02 -0500

    Add overrides

--- nfl-optimizer-backend/ff_overrides.py
+++ nfl-optimizer-backend/ff_overrides.py
@@ -1,2 +1,10 @@
 player_to_pts_override = {
+    # J Jennings
+    "40151274": 12.5,
+    # J Meyers
+    "40151256": 13.0,
+    # J Tolbert
+    "40151386": 8.0,
+    # Puka
+    "40151190": 22.0,
 }
