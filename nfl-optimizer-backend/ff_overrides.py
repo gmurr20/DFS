75efbf655b393f7013b016a497da43bc52d4ad9b
gmurr20 <greg.murray20@gmail.com>
2025-10-14 17:36:56 -0500

    Week 7 salaries

--- nfl-optimizer-backend/ff_overrides.py
+++ nfl-optimizer-backend/ff_overrides.py
@@ -1,10 +1,3 @@
 player_to_pts_override = {
-    # J Jennings
-    "3886598": 12.5,
-    # J Meyers
-    "3916433": 13.0,
-    # J Tolbert
-    "4249417": 8.0,
-    # Puka
-    "4426515": 22.0,
+
 }
