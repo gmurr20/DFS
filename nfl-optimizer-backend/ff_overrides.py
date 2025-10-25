20a406d04e234f4535a637cbf716f938c1127020
gmurr20 <greg.murray20@gmail.com>
2025-10-25 10:22:26 -0500

    dfs week 8 updates

--- nfl-optimizer-backend/ff_overrides.py
+++ nfl-optimizer-backend/ff_overrides.py
@@ -1,2 +1,24 @@
 player_to_pts_override = {
+    # Kirk
+    "14880": 15.12,
+    # Penix
+    "4360423": 0,
+    # Bijan
+    "4430807": 24.5,
+    # D Henry
+    "3043078": 17.0,
+    # CMC
+    "3117251": 22.5,
+    # J Taylor
+    "4242335": 24.0,
+    # J Chase
+    "4362628": 24.0,
+    # W Robinson
+    "4569587": 13.1,
+    # J Higgins
+    "4877706": 9.5,
+    # Ayomanor
+    "4883647": 9.5,
+    # Westbrook Ikhine
+    "3929785": 4.0
 }
